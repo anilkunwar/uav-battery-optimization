f9b7e473ebb914efec1db2738d725eb4cb474e63
Anil Kunwar <anilkunwar318@yahoo.com>
2026-08-16 02:26:48 +0200

    Update model_LiPo_isolated_cell_r7.py

--- thermal_runaway/model_LiPo_isolated_cell_r7.py
+++ thermal_runaway/model_LiPo_isolated_cell_r7.py
@@ -1807,7 +1807,7 @@ if operation_mode == "Run New Simulation":
 
             with tab1:
                 st.markdown("**Multiple cross‑sections clearly show the computational mesh.**")
-                n_slices = st.slider("Number of Z-slices", 2, 100, 50, key='n_slices')
+                n_slices = st.slider("Number of Z-slices", 2, 1000, 500, key='n_slices')
                 fig_ms = create_multi_slice_3d_visualization(T_final, ext, advanced_styling, n_slices=n_slices)
                 st.plotly_chart(fig_ms, use_container_width=True)
 
